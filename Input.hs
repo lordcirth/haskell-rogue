431a11a091020831c95e1b786447633873d1852a
Nathan Fish <lordcirth@gmail.com>
2016-10-17 19:47:13 -0400

    Input now has a default case of 'do nothing'

--- Input.hs
+++ Input.hs
@@ -13,8 +13,10 @@ import qualified Brick.Main as BMain
 import qualified Brick.Types as T
 
 handleInput :: GS.GameState -> V.Event -> T.EventM () (T.Next (GS.GameState))
-handleInput d ev =
+handleInput gs ev =
     case ev of
-        V.EvKey V.KEsc [] -> BMain.halt d
-        V.EvKey V.KEnter [] -> BMain.halt d
-        -- _ -> BMain.continue =<< D.handleDialogEvent ev d
+        V.EvKey V.KEsc [] -> BMain.halt gs
+        V.EvKey V.KEnter [] -> BMain.halt gs
+
+        --else do nothing
+        _ -> BMain.continue gs
