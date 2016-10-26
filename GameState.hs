827661871622304e5274e415c01bfb1db1e22ad9
Nathan Fish <lordcirth@gmail.com>
2016-10-25 22:17:25 -0400

    Add basic Player to Gamestate
    Need to fix other files to match

--- GameState.hs
+++ GameState.hs
@@ -5,9 +5,9 @@ import qualified Data.Map as M
 
 
 -- a square of the board
-data Tile = Tile { walkable :: Bool
-                 , display  :: Char
-                 }
+data Tile = Tile    { walkable :: Bool
+                    , tDisplay  :: Char
+                    }
 
 -- the game emptyBoard / grid
 data Board = Board  { x     :: Int
@@ -16,9 +16,17 @@ data Board = Board  { x     :: Int
                     }
 
 -- The entire game state
-data GameState = GameState { gameBoard  :: Board
-                           , turnNum    :: Int
-                          }
+data GameState = GameState  { gameBoard :: Board
+                            , turnNum   :: Int
+                            , player    :: Player
+                            }
+
+data Player = Player        { cInfo     :: CreatureInfo
+                            }
+
+data CreatureInfo = CreatureInfo    { position  :: (Int, Int)
+                                    , cDisplay  :: Char
+                                    }
 
 floorTile   :: Tile
 floorTile   = Tile True '.'
@@ -32,5 +40,17 @@ emptyBoard sizeX sizeY = Board sizeX sizeY $
     M.fromList (zip pairs (repeat floorTile) )
     where pairs = [ (x,y) | x <- [1..sizeX], y <- [1..sizeY] ]
 
+initialPlayer :: Player
+initialPlayer = Player  { cInfo =
+                          CreatureInfo  { position = (4,4)
+                                        , cDisplay = '@'
+                                        }
+                        }
+
 initialState :: GameState
-initialState = GameState { gameBoard = ( emptyBoard 16 16), turnNum = 0 }
+initialState = GameState    { gameBoard = ( emptyBoard 16 16)
+                            , turnNum = 0
+                            , player = initialPlayer
+                            }
+
+
