de0b8d7183db62719783b131114023785a44b351
Nathan Fish <lordcirth@gmail.com>
2016-11-16 23:26:13 -0500

    switch GameState to use Lenses
    GameState compiles, the rest won't.

--- GameState.hs
+++ GameState.hs
@@ -1,32 +1,47 @@
+{-# Language TemplateHaskell #-} -- For Lenses
+
 -- export everything
 module GameState where
 
 import qualified Data.Map as M
+import Control.Lens
 
+
+--TODO: Start using Lenses!
+
+-- Board stuff:
 
 -- a square of the board
-data Tile = Tile    { walkable :: Bool
-                    , tDisplay  :: Char
+data Tile = Tile    { _walkable :: Bool
+                    , _tDisplay  :: Char
                     }
 
 -- the game emptyBoard / grid
-data Board = Board  { x     :: Int
-                    , y     :: Int
-                    , tiles :: M.Map (Int, Int) Tile
+data Board = Board  { _x     :: Int
+                    , _y     :: Int
+                    , _tiles :: M.Map (Int, Int) Tile
                     }
 
--- The entire game state
-data GameState = GameState  { gameBoard :: Board
-                            , turnNum   :: Int
-                            , player    :: Player
-                            }
+-- Creatures:
 
-data Player = Player        { cInfo     :: CreatureInfo
-                            }
-
-data CreatureInfo = CreatureInfo    { position  :: (Int, Int)
-                                    , cDisplay  :: Char
+data CreatureInfo = CreatureInfo    { _position  :: (Int, Int)
+                                    , _cDisplay  :: Char
                                     }
+
+data Player = Player        { _cInfo     :: CreatureInfo
+                            }
+
+-- The entire game state
+data GameState = GameState  { _gameBoard :: Board
+                            , _turnNum   :: Int
+                            , _player    :: Player
+                            }
+
+makeLenses '' Tile
+makeLenses '' Board
+makeLenses '' GameState
+makeLenses '' Player
+makeLenses '' CreatureInfo
 
 floorTile   :: Tile
 floorTile   = Tile True '.'
@@ -41,16 +56,20 @@ emptyBoard sizeX sizeY = Board sizeX sizeY $
     where pairs = [ (x,y) | x <- [1..sizeX], y <- [1..sizeY] ]
 
 initialPlayer :: Player
-initialPlayer = Player  { cInfo =
-                          CreatureInfo  { position = (4,4)
-                                        , cDisplay = '@'
+initialPlayer = Player  { _cInfo =
+                          CreatureInfo  { _position = (4,4)
+                                        , _cDisplay = '@'
                                         }
                         }
 
 initialState :: GameState
-initialState = GameState    { gameBoard = ( emptyBoard 16 16)
-                            , turnNum = 0
-                            , player = initialPlayer
+initialState = GameState    { _gameBoard = ( emptyBoard 16 16)
+                            , _turnNum = 0
+                            , _player = initialPlayer
                             }
 
+-- shortcut functions
 
+-- Apply a function to every tile and return the new GameState
+forAllTilesDo :: (Tile -> Tile) -> GameState -> GameState
+forAllTilesDo func gs = over (gameBoard.tiles) (fmap func) gs
