2df3767cf6b2f957c01e5449be9fc71b8a789448
Nathan <lordcirth@gmail.com>
2020-01-26 21:44:07 -0500

    vecUnit works
    probably rather weirdly

--- src/Vector.hs
+++ src/Vector.hs
@@ -1,12 +1,16 @@
 module Vector where
 
-newtype Vec2D = Vec2D (Int, Int)
+type Vec2D = (Int, Int)
 
 vecAdd :: Vec2D -> Vec2D -> Vec2D
 vecAdd (x1, y1) (x2, y2) = (x1 + x2, y1 + y2)
 
 -- get a unit vector (rounded heavily into Ints)
 vecUnit :: Vec2D -> Vec2D
-vecUnit (x, y) = (x*magnitude, y*magnitude)
+vecUnit (ix, iy) = (newX, newY)
   where
-    magnitude = sqrt (x^2 +y^2)
+    x = fromIntegral ix :: Float
+    y = fromIntegral iy :: Float
+    newX = round $ x/magnitude :: Int
+    newY = round $ y/magnitude :: Int
+    magnitude = sqrt (x^2 + y^2)
